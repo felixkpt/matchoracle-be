2c420cafc204360362ea018f9f867dbd62cf98e4
Felix Kiptoo <felixkpt@gmail.com>
2023-10-20 18:20:38 +0300

    routes fix

--- routes/nested-routes/admin/matches/view/index.route.php
+++ routes/nested-routes/admin/matches/view/index.route.php
@@ -4,7 +4,4 @@ use App\Http\Controllers\Admin\Matchs\View\MatchController;
 use Illuminate\Support\Facades\Route;
 
 $controller = MatchController::class;
-Route::get('/{year}/{id}', [$controller, 'index'])->name('index');
-Route::post('/{year}/{id}', [$controller, 'store'])->name('store');
-Route::put('/{year}/{id}', [$controller, 'update'])->name('update');
-Route::delete('/{year}/{id}', [$controller, 'destroy'])->name('destroy');
+Route::get('/{id}', [$controller, 'show']);
--- routes/nested-routes/admin/predictions/view/index.route.php
+++ routes/nested-routes/admin/predictions/view/index.route.php
@@ -4,7 +4,4 @@ use App\Http\Controllers\Admin\Predictions\View\PredictionController;
 use Illuminate\Support\Facades\Route;
 
 $controller = PredictionController::class;
-Route::get('/{year}/{id}', [$controller, 'index'])->name('index');
-Route::post('/{year}/{id}', [$controller, 'store'])->name('store');
-Route::put('/{year}/{id}', [$controller, 'update'])->name('update');
-Route::delete('/{year}/{id}', [$controller, 'destroy'])->name('destroy');
+Route::get('/{id}', [$controller, 'show']);
