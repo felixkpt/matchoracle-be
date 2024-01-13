ba95efe250b199cc743a6c666d9b9f209aee555f
Felix Kiptoo <felixkpt@gmail.com>
2024-01-13 09:50:46 +0300

    created use dates hook

--- src/Pages/Admin/Odds/Index.tsx
+++ src/Pages/Admin/Odds/Index.tsx
@@ -2,53 +2,18 @@ import Error404 from '@/Pages/ErrorPages/Error404';
 import AutoTable from '@/components/AutoTable';
 import useListSources from '@/hooks/apis/useListSources';
 import useRouteParamValidation from '@/hooks/useRouteParamValidation';
-import { useEffect, useState } from 'react';
 import "react-datepicker/dist/react-datepicker.css";
-import FormatDate from '@/utils/FormatDate';
-import { useLocation, useNavigate } from 'react-router-dom';
-import MatchesPageHeader from '../../../components/Matches/MatchesPageHeader';
+import MatchesPageHeader from '@/components/Matches/MatchesPageHeader';
 import { oddsColumns } from '@/utils/constants';
+import useFromToDates from '@/hooks/useFromToDates';
 
 const Index = () => {
 
     const { competitions: list_sources } = useListSources()
 
-    const location = useLocation();
-    const navigate = useNavigate();
-
     const errorsState = useRouteParamValidation();
+    const { fromToDates, setFromToDates, baseUri, previousUrl } = useFromToDates('/admin/odds/');
 
-    const [baseUri, setBaseUri] = useState(`/admin/odds/`)
-    const [previousUrl, setPreviousUrl] = useState<string | null>(null)
-
-    useEffect(() => {
-
-        let url = location.pathname
-        setBaseUri(url ? `${url}` : `/admin/odds/`)
-
-        if (previousUrl !== location.pathname) {
-            setPreviousUrl(location.pathname)
-        }
-
-    }, [location.pathname]);
-
-    const initialDates: Array<string | undefined> = [FormatDate.YYYYMMDD(new Date()), undefined];
-    const [fromToDates, setFromToDates] = useState<Array<string | undefined>>(initialDates);
-
-    useEffect(() => {
-        let combinedDates = '';
-        if (fromToDates[0]) {
-            combinedDates = fromToDates[0];
-            if (fromToDates[1]) {
-                if (combinedDates != fromToDates[1])
-                    combinedDates = `${combinedDates}/to/${fromToDates[1]}`;
-
-                const newUrl = `/admin/odds/${combinedDates}`;
-                navigate(newUrl);
-            }
-        }
-    }, [fromToDates, history])
-   
     return (
         <div>
             {
