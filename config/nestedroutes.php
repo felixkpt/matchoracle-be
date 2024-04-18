c234b9bd20f0913b953b795c09012e8ab39e7c8c
Felix Biwott <felixkpt@gmail.com>
2024-04-18 21:29:18 +0300

    updated nestedroutes package

---
+++ config/nestedroutes.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  'folder' => 'nested-routes',
+  'permissions' => 
+  array (
+    'ignored_folders' => 
+    array (
+      0 => 'auth',
+      1 => 'client',
+    ),
+  ),
+  'rename_main_folders' => 
+  array (
+    'admin' => 'dashboard',
+  ),
+);
